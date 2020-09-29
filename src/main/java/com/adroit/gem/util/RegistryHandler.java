e851b6136b011eb458f16416156ca6b16e7dd2fc
AJ <ayaan.jabbar03@gmail.com>
2020-09-29 10:21:53 -0400

    Fifth Commit

--- src/main/java/com/adroit/gem/util/RegistryHandler.java
+++ src/main/java/com/adroit/gem/util/RegistryHandler.java
@@ -1,12 +1,14 @@
 package com.adroit.gem.util;
 
 import com.adroit.gem.Gem;
+import com.adroit.gem.armor.ModArmorMaterial;
 import com.adroit.gem.blocks.BlockItemBase;
 import com.adroit.gem.blocks.RubyBlock;
 import com.adroit.gem.blocks.RubyOre;
 import com.adroit.gem.items.ItemBase;
 import com.adroit.gem.tools.ModItemTier;
 import net.minecraft.block.Block;
+import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.*;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -29,21 +31,31 @@ public class RegistryHandler
     public static final RegistryObject<Item> RUBY = ITEMS.register("ruby", ItemBase::new);
 
     //Tools
-    public static final RegistryObject<SwordItem> RUBY_SWORD = ITEMS.register("ruby", () ->
+    public static final RegistryObject<SwordItem> RUBY_SWORD = ITEMS.register("ruby_sword", () ->
         new SwordItem(ModItemTier.RUBY, 5, -2.4F, new Item.Properties().group(Gem.TAB))
     );
-    public static final RegistryObject<PickaxeItem> RUBY_PICKAXE = ITEMS.register("ruby", () ->
+    public static final RegistryObject<PickaxeItem> RUBY_PICKAXE = ITEMS.register("ruby_pickaxe", () ->
             new PickaxeItem(ModItemTier.RUBY, 3, -2.8F, new Item.Properties().group(Gem.TAB))
     );
-    public static final RegistryObject<ShovelItem> RUBY_SHOVEL = ITEMS.register("ruby", () ->
+    public static final RegistryObject<ShovelItem> RUBY_SHOVEL = ITEMS.register("ruby_shovel", () ->
             new ShovelItem(ModItemTier.RUBY, 3.5F, -3.0F, new Item.Properties().group(Gem.TAB))
     );
-    public static final RegistryObject<AxeItem> RUBY_AXE = ITEMS.register("ruby", () ->
+    public static final RegistryObject<AxeItem> RUBY_AXE = ITEMS.register("ruby_axe", () ->
             new AxeItem(ModItemTier.RUBY, 7, -3.0F, new Item.Properties().group(Gem.TAB))
     );
-    public static final RegistryObject<HoeItem> RUBY_HOE = ITEMS.register("ruby", () ->
+    public static final RegistryObject<HoeItem> RUBY_HOE = ITEMS.register("ruby_hoe", () ->
             new HoeItem(ModItemTier.RUBY, 0.0F, new Item.Properties().group(Gem.TAB))
     );
+
+    //Armor
+    public static final RegistryObject<ArmorItem> RUBY_HELMET = ITEMS.register("ruby_helmet",
+            () -> new ArmorItem(ModArmorMaterial.RUBY, EquipmentSlotType.HEAD, new Item.Properties().group(Gem.TAB)));
+    public static final RegistryObject<ArmorItem> RUBY_CHESTPLATE = ITEMS.register("ruby_chestplate",
+            () -> new ArmorItem(ModArmorMaterial.RUBY, EquipmentSlotType.CHEST, new Item.Properties().group(Gem.TAB)));
+    public static final RegistryObject<ArmorItem> RUBY_LEGGINGS = ITEMS.register("ruby_leggings",
+            () -> new ArmorItem(ModArmorMaterial.RUBY, EquipmentSlotType.LEGS, new Item.Properties().group(Gem.TAB)));
+    public static final RegistryObject<ArmorItem> RUBY_BOOTS = ITEMS.register("ruby_boots",
+            () -> new ArmorItem(ModArmorMaterial.RUBY, EquipmentSlotType.FEET, new Item.Properties().group(Gem.TAB)));
 
     //Blocks
     public static final RegistryObject<Block> RUBY_BLOCK = BLOCKS.register("ruby_block", RubyBlock::new);
